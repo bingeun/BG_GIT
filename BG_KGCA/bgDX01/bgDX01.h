c29763640aff642a84704de1fc115adb7ebe1835
BG <bingeun@dreamwiz.com>
2016-06-15 15:55:55 +0900

    육면체 출력, 회전, fmod 폴더 이동

--- BG_KGCA/bgDX01/bgDX01.h
+++ BG_KGCA/bgDX01/bgDX01.h
@@ -1,9 +1,24 @@
 #pragma once
 #include <d3d11.h>
 #include <d3dx11.h>
+#include <d3dcompiler.h>
+#include <xnamath.h>
 #include "bgStd.h"
 #include "bgSys.h"
 #include "bgCore.h"
+
+struct SimpleVertex
+{
+	XMFLOAT3 Pos;
+	XMFLOAT4 Color;
+};
+
+struct ConstantBuffer
+{
+	XMMATRIX mWorld;
+	XMMATRIX mView;
+	XMMATRIX mProjection;
+};
 
 D3D_DRIVER_TYPE         g_driverType = D3D_DRIVER_TYPE_NULL;
 D3D_FEATURE_LEVEL       g_featureLevel = D3D_FEATURE_LEVEL_11_0;
@@ -11,6 +26,15 @@ ID3D11Device*           g_pd3dDevice = NULL;
 ID3D11DeviceContext*    g_pImmediateContext = NULL;
 IDXGISwapChain*         g_pSwapChain = NULL;
 ID3D11RenderTargetView* g_pRenderTargetView = NULL;
+ID3D11VertexShader*     g_pVertexShader = NULL;
+ID3D11PixelShader*      g_pPixelShader = NULL;
+ID3D11InputLayout*      g_pVertexLayout = NULL;
+ID3D11Buffer*           g_pVertexBuffer = NULL;
+ID3D11Buffer*           g_pIndexBuffer = NULL;
+ID3D11Buffer*           g_pConstantBuffer = NULL;
+XMMATRIX                g_World;
+XMMATRIX                g_View;
+XMMATRIX                g_Projection;
 
 HRESULT InitDevice();
 void CleanupDevice();
